27337a8b14263f4b44f2106cb57d11a56d59ac14
Patrick BODET <patrickbodet@MacBook-Pro-de-Patrick.local>
2014-09-24 14:41:52 +0200

    Initial Commit

---
+++ TestSMCallout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestSMCallout
+//
+//  Created by Patrick BODET on 24/09/2014.
+//  Copyright (c) 2014 Patrick BODET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestSMCallout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestSMCallout
+//
+//  Created by Patrick BODET on 24/09/2014.
+//  Copyright (c) 2014 Patrick BODET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
