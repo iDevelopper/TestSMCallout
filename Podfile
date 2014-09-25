f0ae0a31dd9ecf137cfd254839d5a8f642cf09ac
iDevelopper <patrick.bodet@wanadoo.fr>
2014-09-25 08:07:12 +0200

    Test SMCalloutView
    contentView display issue

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, "7.0"
+
+target "TestSMCallout" do
+pod 'SMCalloutView', '~> 2.0'
+end
+
+target "TestSMCalloutTests" do
+
+end
+
--- TestSMCallout/ViewController.m
+++ TestSMCallout/ViewController.m
@@ -6,9 +6,13 @@
 //  Copyright (c) 2014 Patrick BODET. All rights reserved.
 //
 
+#import "SMCalloutView.h"
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () {
+
+    UIButton *button;
+}
 
 @end
 
@@ -17,11 +21,49 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor blackColor];
+    
+    button = [UIButton buttonWithType:UIButtonTypeCustom];
+    UIImage *image = [UIImage imageNamed:@"Pin.png"];
+    button.center = self.view.center;
+    button.frame = CGRectMake(self.view.center.x - image.size.width / 2, self.view.center.y - image.size.height / 2, image.size.width, image.size.height);
+    NSLog(@"Button Frame = %@",NSStringFromCGRect(button.frame));
+    [button setImage:image forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(markerClicked:) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.view addSubview:button];
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)markerClicked:(id)sender {
+    UIView *contentView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 200, 200)];
+    contentView.backgroundColor = [UIColor yellowColor];
+    
+    UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(10, 50, 100, 25)];
+    label.backgroundColor = [UIColor clearColor];
+    label.text = @"SMCallout Sample";
+    [contentView addSubview:label];
+    
+    SMCalloutView *calloutView = [[SMCalloutView alloc]init];
+    calloutView.contentView = contentView;
+    
+    UIButton *accessoryButton = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+    [accessoryButton addTarget:self action:@selector(tapOnCalloutAccessoryControl:) forControlEvents:UIControlEventTouchUpInside];
+    calloutView.rightAccessoryView = accessoryButton;
+    calloutView.permittedArrowDirection = SMCalloutArrowDirectionAny;
+    
+    [calloutView presentCalloutFromRect:button.frame inView:self.view constrainedToView:self.view animated:YES];
+}
+
+- (void)tapOnCalloutAccessoryControl:(UIControl *)control
+{
+    UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"SMCallout" message:@"AccessoryView clicked" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+    [alert show];
 }
 
 @end
